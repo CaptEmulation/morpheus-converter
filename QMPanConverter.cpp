db8ee02d2bd02373ee3d1e6d8e64fff7b8138ff0
John Dean <john.holmes.dean@gmail.com>
2013-12-08 23:55:53 -0600

    Improved conversion  - Instead of converting images while movie is playing, cache the frames in memory to convert afterwards  - Fixes skipped/duped frames during conversion on slow systems

--- QMPanConverter.cpp
+++ QMPanConverter.cpp
@@ -55,6 +55,7 @@ public:
     qint32 mCount;
     QProgressBar *mProgressBar;
     QMFrameSoftwareRenderYuv *mFrameConverter;
+    QList<QVideoFrame> mFrames;
 
     void initialize() {
         log = Log4Qt::Logger::logger("QMPanConverter");
@@ -73,43 +74,49 @@ public:
     }
 
 
+    void convertAllFrames() {
+        mProgressBar->setMaximum(mFrames.length() - 1);
+        int count = 0;
+
+        foreach(QVideoFrame frame, mFrames) {
+            mProgressBar->setValue(count++);
+            QImage image;
+            if (frame.pixelFormat() == QVideoFrame::Format_RGB32) {
+                // Copy const QVideoFrame to mutable QVideoFrame.
+                QVideoFrame nonConstFrame = frame;
+                // Unlock for reading the stack frame (increment ref pointer)
+                nonConstFrame.map(QAbstractVideoBuffer::ReadOnly);
+                // Create new image from the frame bits
+                image = QImage(
+                        nonConstFrame.bits(),
+                        nonConstFrame.width(),
+                        nonConstFrame.height(),
+                        nonConstFrame.bytesPerLine(),
+                        QVideoFrame::imageFormatFromPixelFormat(nonConstFrame.pixelFormat()));
+                nonConstFrame.unmap();
+            } else {
+                image = QImage(frame.size(), QImage::Format_RGB32);
+                mFrameConverter->convertFrame(frame, &image);
+            }
+
+            QString imgFileName = QString("%1.%2.png").arg(mFileName).arg(++mCount, 2, 10, QChar('0'));
+            //QFile file(imgFileName);
+            //file.open(QFile::WriteOnly);
+
+            bool saved = image.save(imgFileName, "png");
+            if (saved) {
+                log->info("File: %1 saved", imgFileName);
+            } else {
+                log->info("File: %1 not saved", imgFileName);
+            }
+
+        }
+    }
 
     void videoFrameLoaded(const QVideoFrame &frame) {
         log->trace("ENTER videoFrameLoaded");
 
-
-        log->debug("Pixel format: %1", frame.pixelFormat());
-        QImage image;
-        if (frame.pixelFormat() == QVideoFrame::Format_RGB32) {
-            // Copy const QVideoFrame to mutable QVideoFrame.
-            QVideoFrame nonConstFrame = frame;
-            // Unlock for reading the stack frame (increment ref pointer)
-            nonConstFrame.map(QAbstractVideoBuffer::ReadOnly);
-            // Create new image from the frame bits
-            image = QImage(
-                    nonConstFrame.bits(),
-                    nonConstFrame.width(),
-                    nonConstFrame.height(),
-                    nonConstFrame.bytesPerLine(),
-                    QVideoFrame::imageFormatFromPixelFormat(nonConstFrame.pixelFormat()));
-            nonConstFrame.unmap();
-        } else {
-            image = QImage(frame.size(), QImage::Format_RGB32);
-            mFrameConverter->convertFrame(frame, &image);
-        }
-        log->debug("File converted");
-
-        QString imgFileName = QString("%1.%2.png").arg(mFileName).arg(++mCount, 2, 10, QChar('0'));
-        //QFile file(imgFileName);
-        //file.open(QFile::WriteOnly);
-
-        bool saved = image.save(imgFileName, "png");
-        if (saved) {
-            log->info("File: %1 saved", imgFileName);
-        } else {
-            log->info("File: %1 not saved", imgFileName);
-        }
-
+        mFrames.append(frame);
 
         log->trace("LEAVE videoFrameLoaded");
     }
@@ -122,11 +129,12 @@ public:
         mFileName = url.toLocalFile();
         //mFileName.chop(4);
         mCount = 0;
-        mMediaPlayer->setNotifyInterval(1000 / 60);
+        //mMediaPlayer->setNotifyInterval(1000 / 60);
         mMediaPlayer->setMedia(url);
         mMediaPlayer->setMuted(true);
         mMediaPlayer->setPlaybackRate(1.0);
         mMediaPlayer->play();
+        mFrames = QList<QVideoFrame>();
     }
 
      void positionChanged(int pos) {
@@ -134,11 +142,13 @@ public:
         if (duration && mProgressBar) {
             mProgressBar->setRange(0, duration);
             mProgressBar->setValue(pos);
+            log->info("Media position changed: %1 of %2", pos, duration);
         }
      }
 
      void mediaStatusChanged(QMediaPlayer::MediaStatus status) {
          if (status == QMediaPlayer::EndOfMedia || status == QMediaPlayer::InvalidMedia) {
+             convertAllFrames();
              emit self->done();
          }
      }
